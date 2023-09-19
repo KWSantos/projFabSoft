492d20c979a89e808c78b2587869000322c1f4b9
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2023-09-18 22:10:25 -0300

    Listagem de Dados no Dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
@@ -30,6 +31,9 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
+        // $user = User::find($request->id);
+        // dd ($user);
+        // return redirect()->intended('dashboard', compact('user'));
     }
 
     /**
